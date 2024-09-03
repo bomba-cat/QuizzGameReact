3e78f8b06b44f2f52386b67686f07514a373318b
Zer0 <max.hasselwander@wiss-edu.ch>
2024-09-03 10:41:54 +0200

    tmp

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,6 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className="p-48 bg-gradient-to-tr from-zinc-100 via-gray-200 to-slate-300 dark:bg-gradient-to-tr dark:from-slate-900 dark:via-gray-800 dark:to-zinc-700 h-screen w-screen absolute top-0 left-0">
-        <h1 className="absolute top-36 -translate-y-1/2 -translate-x-1/2 left-1/2">Waehlen sie ein Quiz</h1>
-        <div className="answer-buttons m-1 absolute inset-y-2/4 -translate-y-1/2 -translate-x-1/2 left-1/2">
-          {option_buttons}
-        </div>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<div>Choose a quiz</div>} />
@@ -28,7 +23,6 @@ function App() {
               <Route path="gpu" element={<GPU />} />
           </Route>
         </Routes>
-      </div>
     </BrowserRouter>
   )
 }
