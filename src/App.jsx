d34f0bd8d438da0762178b81bb505d1163845982
bomba-cat <bledion.morina@hotmail.com>
2024-09-03 10:44:11 +0200

    const copied to layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,6 @@ import Layout from './components/layout.jsx'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const options = ["autos", "cpu", "gpu"]
-  const option_buttons = options.map(a =>
-    <Category key={a} s="m-8 p-4 font-mono text-6xl" name={a} />
-  )
-
   return (
     <BrowserRouter>
       <div className="p-48 bg-gradient-to-tr from-zinc-100 via-gray-200 to-slate-300 dark:bg-gradient-to-tr dark:from-slate-900 dark:via-gray-800 dark:to-zinc-700 h-screen w-screen absolute top-0 left-0">
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
-	return (<div className="App">
+  const options = ["autos", "cpu", "gpu"]
+  const option_buttons = options.map(a =>
+    <Category key={a} s="m-8 p-4 font-mono text-6xl" name={a} />
+  )
+
+  return (<div className="App">
 			<h1 className="absolute top-36 -translate-y-1/2 -translate-x-1/2 left-1/2">Waehlen sie ein Quiz</h1>
       <div className="answer-buttons m-1 absolute inset-y-2/4 -translate-y-1/2 -translate-x-1/2 left-1/2">
       	{option_buttons}
