7f60b3e5933955770170c0ae78285591779252ae
bomba-cat <bledion.morina@hotmail.com>
2024-09-03 09:26:37 +0200

    button style

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
   const options = ["autos", "cpu", "gpu"]
   const option_buttons = options.map(a =>
-    <Category key={a} name={a} />
+    <Category key={a} s="m-8 p-4 font-mono text-6xl" name={a} />
   )
 
   return (
