d5136b6507c955d08b2fb39cbbaa3f7464f61bfe
bomba-cat <bledion.morina@hotmail.com>
2024-09-03 10:40:42 +0200

    update

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,10 @@ import { Outlet } from 'react-router-dom'
 
 export default function Layout() {
 	return (<div className="App">
+			<h1 className="absolute top-36 -translate-y-1/2 -translate-x-1/2 left-1/2">Waehlen sie ein Quiz</h1>
+      <div className="answer-buttons m-1 absolute inset-y-2/4 -translate-y-1/2 -translate-x-1/2 left-1/2">
+      	{option_buttons}
+      </div>
 			<Outlet />
 		</div>)
 }
