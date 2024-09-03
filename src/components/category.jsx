2a4aae7a54ee50800aea3659ba7973484a61f6c3
bomba-cat <bledion.morina@hotmail.com>
2024-09-03 09:59:51 +0200

    updat

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Category(props) {
 	return (<>
-		<Link className={`${ props.s }`} to={`/${ props.name.toLowerCase()} `}>
+		<Link className={ props.s } to={`/${ props.name.toLowerCase()} `}>
       { props.name }
     </Link>
 	</>)
